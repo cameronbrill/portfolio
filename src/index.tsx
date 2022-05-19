8e47a55c91e0f8ad16ff75b24813d0a8b5cc0455
Cameron Brill <c00brill@gmail.com>
2022-05-19 14:48:30 -0700

    feat: install logrocket

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,12 @@ import Home from "@Pages/home";
 import "./index.scss";
 import { BrowserRouter as Router } from "react-router-dom";
 import { H } from "highlight.run";
+import LogRocket from "logrocket";
 
 H.init(process.env.REACT_APP_HIGHLIGHT_PROJ_ID, {
   environment: process.env.NODE_ENV,
 });
+LogRocket.init(`${process.env.REACT_APP_LOG_ROCKET_PROJ_ID}`);
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
