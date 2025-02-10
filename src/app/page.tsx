da36a5c2a8cca3c1383b2099ff9e6dec080ead8e
Cameron <c00brill@gmail.com>
2025-02-10 17:06:44 +0900

    add mobile interactivity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,41 @@
 "use client";
 
+import { useKBar } from "kbar";
 import React, { useEffect } from "react";
 
 import styles from "./page.module.scss";
 
-const getOs = () => {
+const isMobile = (): boolean => {
+  let hasTouchScreen = false;
+
+  if ("maxTouchPoints" in navigator) {
+    hasTouchScreen = navigator.maxTouchPoints > 0;
+  } else if ("msMaxTouchPoints" in navigator) {
+    // @ts-ignore
+    hasTouchScreen = navigator.msMaxTouchPoints > 0;
+  } else {
+    // @ts-ignore
+    const mQ = window.matchMedia && matchMedia("(pointer:coarse)");
+    if (mQ && mQ.media === "(pointer:coarse)") {
+      hasTouchScreen = !!mQ.matches;
+    } else if ("orientation" in window) {
+      hasTouchScreen = true; // deprecated, but good fallback
+    } else {
+      // Only as a last resort, fall back to user agent sniffing
+      hasTouchScreen =
+        /\b(BlackBerry|webOS|iPhone|IEMobile|Android|Windows Phone|iPad|iPod)\b/i.test(
+          // @ts-ignore
+          navigator.userAgent,
+        );
+    }
+  }
+  return hasTouchScreen;
+};
+
+const getTriggerMessage = () => {
+  if (isMobile()) {
+    return "Tap anywhere";
+  }
   if (navigator.userAgent.includes("Mac")) {
     return "⌘+K";
   }
@@ -12,15 +43,31 @@ const getOs = () => {
 };
 
 const Home = () => {
-  const [os, setOs] = React.useState<string>("");
+  const [triggerMessage, setTriggerMessage] = React.useState<string>("");
 
   useEffect(() => {
-    setOs(getOs());
+    setTriggerMessage(getTriggerMessage());
   }, []);
 
+  const { query } = useKBar();
+
+  const shouldAllowNonKeyboardInteractivity = isMobile();
+
+  const handleClick = () => {
+    if (shouldAllowNonKeyboardInteractivity) {
+      query.toggle();
+    }
+  };
+
   return (
-    <div role="heading" aria-level={1} className={styles.cta}>
-      {os}
+    // biome-ignore lint/a11y/useKeyWithClickEvents: keyboard interactivity is handled by KBar
+    <div
+      role="heading"
+      aria-level={1}
+      className={styles.cta}
+      onClick={handleClick}
+    >
+      {triggerMessage}
     </div>
   );
 };
